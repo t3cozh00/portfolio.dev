6c6e3418cc85dc9f7c87a1617b05a1a514635529
Congying Zhao <t3cozh00@students.oamk.fi>
2025-10-26 21:34:17 +0200

    initialize portfolio

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutPage = () => {
+  return <div className="text-white text-5xl">AboutPage</div>;
+};
+
+export default AboutPage;
---
+++ components/Home/Navbar/MobileNav.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavLinks } from "@/constant/constant";
+
+import Link from "next/link";
+import { CgClose } from "react-icons/cg";
+
+type Props = {
+  showNav: boolean;
+  closeNav: () => void;
+};
+
+const MobileNav = ({ closeNav, showNav }: Props) => {
+  const navOpen = showNav ? "translate-x-0" : "translate-x-[100%]";
+  return (
+    <div>
+      {/* overlay */}
+      <div
+        className={`fixed inset-0 ${navOpen}transform transition-all right-0 duration-500 z-[100002] bg-black opacity-70 w-full h-screen`}
+      ></div>
+      {/* navlinks */}
+      <div
+        className={`text-white ${navOpen} fixed justify-center flex-col h-full transform transition-500 delay-300 w-[80%] sm:w-[60%] bg-cyan-800 space-y-6 z-[100050] right-0`}
+      >
+        {NavLinks.map((link) => {
+          return (
+            <Link key={link.id} href={link.url}>
+              <p className="text-white w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]">
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+        {/* cross icon */}
+        <CgClose
+          onClick={closeNav}
+          className="absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MobileNav;
